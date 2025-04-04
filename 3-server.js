0daf37a54ec44202ef2d0d582966dd238289f0f1
dk <kukkudanish@gmail.com>
2025-04-04 14:36:40 +0530

    first commit

---
+++ 3-server.js
@@ -0,0 +1,65 @@
+// const HTTP = require('http')
+// const server = HTTP.createServer((request , response) =>{
+//     response.writeHead(200 , {'content-type' : 'text/plain'})
+//     response.end("this is our first server calling")
+// })
+// const PORT = 30002
+// server.listen(PORT , ()=>{
+//     console.log('server is running')
+// })
+
+
+
+
+// const HTTP = require('http')
+// const server = HTTP.createServer((request , response) =>{
+//     response.writeHead(200,{'content-type': 'text/plain'})
+//     response.end('https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg')
+// })
+// const  PORT = 3002
+// server.listen(PORT , () =>{
+//     console.log('server  is renning')
+// })
+
+
+
+// const HTTP = require('http')
+// const PORT = 3000 
+
+// const server = HTTP.createServer((request , response) =>{
+//     response.writeHead(200 , {'content-type' : 'text/plain'})
+//     response.end("hy iam danish")
+// }) 
+// server.listen(PORT , () =>{
+//     console.log("sever is running")
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const HTTP = require('http')
+// const PORT = 5002
+
+// const SERVER = HTTP.createServer((request , response) =>{
+//     response.writeHead(200 , {"content-type" : 'text/plain'})
+//     response.end("you know me iam the devil")
+// })
+
+// SERVER.listen(PORT , () =>{
+//     console.log("server is run")
+// })
--- pathModule.js
+++ pathModule.js
@@ -1,3 +1,14 @@
 const PATH =  require('path')
 const fileName  = 'fsSample.txt'
+
+
+//fullPath
 console.log("full path : ", PATH.join(__dirname , fileName))
+
+// directory  path
+console.log("only directry path : ", PATH.dirname('HOME / folder/sample.js'))
+
+// only  extention(.js , .css , .html .....)
+console.log("only extension : " , PATH.extname('home/folder/sample.js'))
+console.log("only extension : " , PATH.extname('home/folder/sample.jsx'))
+console.log("only extension : " , PATH.extname('home/folder/sample.html'))
