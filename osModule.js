5c9d364d568a6ded4e0a432710cefad1c0dfd589
dk <kukkudanish@gmail.com>
2025-04-16 17:01:47 +0530

    ADD

--- osModule.js
+++ osModule.js
@@ -1,17 +1,17 @@
   
 
-//import 
+// //import 
 
-const OS = require('os')
-console.log("type :" ,OS.type())
-console.log("platform :",OS.platform());
-console.log("totalMemory : " , OS.totalmem());
-console.log("Processor Architecture : ",OS.arch())
-console.log("System name : ",OS.hostname())
-console.log("Free Memory : ", OS.freemem());
-console.log('CPU details : ' , OS.cpus())
-console.log("System Load Average : " , OS.loadavg());
-console.log("Network inFORMATION : ",OS.networkInterfaces())
-console.log('loged user :',OS.userInfo())
-console.log('USER form directory : ',OS.homedir());
-console.log("system app time(in seconds) :",OS.uptime())
+// const OS = require('os')
+// console.log("type :" ,OS.type())
+// console.log("platform :",OS.platform());
+// console.log("totalMemory : " , OS.totalmem());
+// console.log("Processor Architecture : ",OS.arch())
+// console.log("System name : ",OS.hostname())
+// console.log("Free Memory : ", OS.freemem());
+// console.log('CPU details : ' , OS.cpus())
+// console.log("System Load Average : " , OS.loadavg());
+// console.log("Network inFORMATION : ",OS.networkInterfaces())
+// console.log('loged user :',OS.userInfo())
+// console.log('USER form directory : ',OS.homedir());
+// console.log("system app time(in seconds) :",OS.uptime())
