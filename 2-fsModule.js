bb669e5d09cb6da3cde4ff5f67144448c8fa305d
dk <kukkudanish@gmail.com>
2025-05-15 11:28:55 +0530

    add

--- 2-fsModule.js
+++ 2-fsModule.js
@@ -58,3 +58,66 @@ const FS = require('fs')
         
 //     }
 // })
+
+
+
+
+// const fs = require('fs')
+// const fileContent =  fs.readFileSync('./write.txt' , "utf-8")
+// console.log(fileContent)
+
+
+// const buf = Buffer.from('hy iam danish fr0o0m manjeri');
+// console.log(buf); 
+
+
+
+// const BufferOfString =  Buffer.from("hy mister dsjon")
+// console.log(BufferOfString)
+
+
+  
+               // this is 2 way of reading a file 
+               // 1 - readFile() (asychronous non-blocking)
+               // 2 - readFileSync() (sychrous blockig)
+               // 3 - createReadStream()  (very large file ashychrious and non-blocking)
+
+
+                // 1 - readFile()
+
+// const fs = require('fs')
+// try {
+//     const data = fs.readFileSync('./fsSample.txt' , 'utf-8' )
+//     console.log( 'reading data \n',data)
+// } catch (error) {
+//     console.log('error reading file' , error)
+// }
+
+
+
+            //2 - readFileSync() 
+
+// const fs = require('fs')
+// fs.readFile('./write.txt' , 'utf-8' , (err , data) =>{
+//     if(err){
+//         console.log(err)
+//         return
+//     }else{
+//         console.log(data)
+//     }
+// })
+
+
+
+                 // 3 - createReadStream() 
+
+// const fs = require('fs')
+// const stream =  fs.createReadStream('./write.txt' , 'utf-8')
+// console.log("first")
+// stream.on('data' , chunk =>{
+//     console.log(chunk)
+// })
+// console.log("seconnd");
+
+
+/////////////////////////////////////////////////////////////////////////////////
